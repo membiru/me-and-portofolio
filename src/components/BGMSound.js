48ec31a6917ed23281cee77096b40476f7930cc4
Rizky Ananda <rizkyananda007@gmail.com>
2022-06-18 13:43:15 +0800

    Add Audio Player
    Add feature to play audio on nav bar

--- src/components/BGMSound.js
+++ src/components/BGMSound.js
@@ -1,27 +1,72 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+import audioSrc from "../assets/audio/Talking-To-The-Moon.mp3"
 
 const Container = styled.div`
     display: flex;
-    left: 8rem;
-    top: 3rem;
-    
+    left: 10rem;
+    top: 3.4rem;
+    cursor: pointer;
+
     position: fixed;
     z-index:10;
+
+    &>*:nth-child(1) {
+        animation-delay: 0.1s;
+    }
+    &>*:nth-child(2) {
+        animation-delay: 0.2s;
+    }
+    &>*:nth-child(3) {
+        animation-delay: 0.3s;
+    }
+    &>*:nth-child(4) {
+        animation-delay: 0.4s;
+    }
 `
 
-const BGMSound = () => {
-    // const ref = React.useRef(null)
-    // const [start, setStart] = React.useState(false)
+const play = keyframes`
+    0%{
+        transform: scaleY(1);
+    }
+    50%{
+        transform: scaleY(1.5);
+    }
+    100%{
+        transform: scaleY(1);
+    }
+`
 
-    // const handleClick = () => {
-    //     setStart(!start)
-    //     !start ? ref.current.play() : ref.current.pause()
-    // }
+const AudioPulseLine = styled.div`
+    border: 2px solid ${props => props.color === 'dark' ? props.theme.secondary : props.theme.primary};
+    background:  ${props => props.color === 'dark' ? props.theme.primary : props.theme.secondary};
+    height: 1.5rem;
+    width: 2px;
+    margin: 0 0.1rem;
+
+    animation: ${play} 1s ease infinite;
+    animation-play-state: ${props => props.isClick ? "running" : "paused"};
+`
+
+
+const BGMSound = () => {
+    const ref = React.useRef(null)
+    const [start, setStart] = React.useState(false)
+
+    const handleClick = () => {
+        setStart(!start)
+        !start ? ref.current.play() : ref.current.pause()
+    }
     
     return (
-        <Container >
-            Sound
+        <Container onClick={handleClick}>
+            <AudioPulseLine isClick={start} />
+            <AudioPulseLine isClick={start} />
+            <AudioPulseLine isClick={start} />
+            <AudioPulseLine isClick={start} />
+
+            <audio src={audioSrc} ref={ref} loop />
         </Container>
     )
 }
